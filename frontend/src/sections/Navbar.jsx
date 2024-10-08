85acfefa58002e4b659de1b35a042bdcdb1e09b0
fady668 <fadygeorge668@gmail.com>
2024-10-08 11:43:19 +0300

    Finish Navbar

--- frontend/src/sections/Navbar.jsx
+++ frontend/src/sections/Navbar.jsx
@@ -2,19 +2,19 @@ import React from "react";
 import logo from "../media/Mahabaprint-img.png";
 
 const Navbar = () => {
-    return (
-        <div className="navbar">
-            <div className="container">
-                <img src={logo} alt="" className="logo" />
-                <div className="links">
-                    <a href="/">Home</a>
-                    <a href="/">Our Business</a>
-                    <a href="/">About Us</a>
-                    <a href="/">Contact Us</a>
-                </div>
-            </div>
+  return (
+    <div className="navbar">
+      <div className="container">
+        <img src={logo} alt="" className="logo" />
+        <div className="links">
+          <a href="/">Home</a>
+          <a href="/">Our Business</a>
+          <a href="/">About Us</a>
+          <a href="/">Contact Us</a>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
